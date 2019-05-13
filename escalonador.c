66d81345cfee5d0d465753ae1cb671a52df6e6ee
Yuri <yuricda96@gmail.com>
2019-05-13 12:00:51 -0300

    ready queue done

--- escalonador.c
+++ escalonador.c
@@ -7,13 +7,17 @@
 #include <sys/types.h>
 #include <sys/msg.h>
 #include <sys/errno.h>
+#include <sys/wait.h>
+#include <time.h>
 
 #include "escalonador.h"
 
 int main(int argc, char const *argv[])
 {
-    int pid, msgid_exec_post, msgid_escale;
+    int pid, msgid_exec_post, msgid_escale, current_time;
     struct msg msg_2_rcv;
+    Queue* ready_queue = NULL;
+ 
     char arg_error_msg[] = "Como argumento insira qual topologia deseja usar no escalonador:\n0: hypercube\n1: torus\n2: fat tree\nSeu comando deve ser: escalonador <topologia> &\n";
 
     if (argc < 2 || (argc > 4 && strcmp(argv[2], "&")))
@@ -43,11 +47,20 @@ int main(int argc, char const *argv[])
         printf("MSG List didnt able to be created");
         exit(1);
     }
+    
+    ready_queue = start_queue();
 
-    // printf("msgid_exec_post = %d\n", msgid_exec_post);
-    // printf("msgid_escale = %d\n", msgid_escale);
+    if(ready_queue == NULL){
+        printf("Fila não criada\n");
+        exit(1);
+    }
 
-    //sleep(10);
+
+
+    printf("msgid_exec_post = %d\n", msgid_exec_post);
+    printf("msgid_escale = %d\n", msgid_escale);
+
+    sleep(10);
 
     Nodo hypercube[16];
     Nodo torus[4][4];
@@ -86,25 +99,83 @@ int main(int argc, char const *argv[])
         break;
     }
 
-    if (pid == 0)
-    {
-    }
-
-    if (pid != 0)
-    {
-        while (1)
-        {
-            printf("passou\n");
+    if (pid != 0){
+        while (1){      
+            
             msgrcv(msgid_exec_post, &msg_2_rcv, sizeof(msg_2_rcv) - sizeof(long), 0, 0);
-            printf("chegou alguma msg %d\n", errno);
-            printf("%ld %s\n", msg_2_rcv.sec, msg_2_rcv.arq_executavel);
+            insert_queue(ready_queue, msg_2_rcv);  
+            print_queue(ready_queue);          
+            // current_time = alarm((int) msg_2_rcv.sec);
+            // if (current_time == 0){
+            //     //executa o prox oque chegou agr
+            // }else if(msg_2_rcv < current_time ){
+            //     ready_queue->init;
+            //     alarm();
+
+            // }
+            
+            
+
+            // printf("chegou alguma msg %d\n", errno);
+            // printf("%ld %s\n", msg_2_rcv.sec, msg_2_rcv.arq_executavel);
+            // if(ready_queue->next == NULL){
+            //     continue;
+            // }
             // print_topology(FATTREE, fattree);
         }
     }
 
+
+    if (pid == 0)
+    {   
+        time_t exec_init;
+        time_t exec_end;
+        struct end_msg msg_2_snd;
+
+        while(1){
+            printf("aqui");
+            msgrcv(msgid_escale, &msg_2_rcv, sizeof(msg_2_rcv) - sizeof(long), 0, 0);
+
+            exec_init = time(NULL);
+            if((pid = fork()) <0){
+                printf("Error on fork() -> %d", errno);
+                continue;
+            }
+            
+            if(pid == 0){
+                char *str;
+                strcpy(str, msg_2_rcv.arq_executavel);
+                int init_size = strlen(str);
+                char delim[] = "/";
+                char *filename;
+
+                char *ptr = strtok(str, delim);
+
+                while (ptr != NULL){
+                    filename = ptr;
+                    ptr = strtok(NULL, delim);
+                }
+                printf("'%s'\n", filename);
+                
+                execl(msg_2_rcv.arq_executavel, filename, (char*) 0);
+            }   
+
+            int state;
+            wait(&state);
+            exec_end = time(NULL);
+            msg_2_snd.pid = getpid();
+            msg_2_snd.exec_time[0] = exec_init;
+            msg_2_snd.exec_time[1] = exec_end;
+            msgsnd(msgid_escale, &msg_2_snd, sizeof(msg_2_snd) - sizeof(long), 0);
+        }
+    }
+    
+    
+
     struct msqid_ds *buf;
     msgctl(msgid_exec_post, IPC_RMID, buf);
     msgctl(msgid_escale, IPC_RMID, buf);
+    free_queue(ready_queue);
 
     return 0;
 }
--- escalonador.h
+++ escalonador.h
@@ -7,10 +7,33 @@
 #define KEY_EXEC_POST 0x03718
 #define KEY_ESCALE 0x6659
 
-struct msg
-{
+struct msg{
     long sec;
     char arq_executavel[100];
+};
+
+struct end_msg{
+    long pid;
+    time_t exec_time[2];
+};
+
+struct queue_nodo{
+    int sec;
+    char arq_executavel[100];
+    struct queue_nodo* next;
+};
+
+struct queue {
+    struct queue_nodo* init;
+};
+typedef struct queue Queue;
+
+
+struct execution_node{
+    long sec;
+    char arq_executavel[100];
+    time_t exec_init;
+    time_t exec_end;
 };
 
 /*Definindo o cabeçalho*/
@@ -50,6 +73,7 @@ struct tree_nodo
 };
 
 typedef struct tree_nodo TreeNodo;
+
 
 void print_topology(int type, TreeNodo *fattree)
 {
@@ -113,4 +137,104 @@ void create_tree(TreeNodo fattree[15])
     }
 }
 
+Queue* start_queue(){
+    Queue* ready_queue = (Queue*) malloc(sizeof(Queue));
+    if(ready_queue != NULL){
+        ready_queue->init = NULL;
+    }
+    return ready_queue;
+}
+
+int is_empty(Queue* ready_queue){
+    if(ready_queue->init == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+int free_queue(Queue* ready_queue){
+    struct queue_nodo* aux, *tmp= ready_queue->init;
+    if(!is_empty(ready_queue)){
+        do{
+            aux = tmp;
+            tmp = tmp->next;
+            free(aux);
+            
+        }while(tmp != NULL);
+    }
+
+}
+int remove_queue(Queue* ready_queue){
+    if(ready_queue == NULL)
+        return 0;
+    if(ready_queue->init == NULL){
+        return 0;
+    }
+    struct queue_nodo* nodo = ready_queue->init;
+
+    ready_queue->init = ready_queue->init->next;
+
+    free(nodo);
+    return 1;
+
+}
+
+int insert_queue(Queue* ready_queue, struct msg insert_msg){
+    if (ready_queue == NULL)
+        return 0;
+    struct queue_nodo* new_nodo = (struct queue_nodo *) malloc(sizeof(struct queue_nodo));
+    
+    if(new_nodo == NULL)
+        return 0;
+    new_nodo->sec = insert_msg.sec; 
+    strcpy(new_nodo->arq_executavel, insert_msg.arq_executavel);
+    new_nodo->next = NULL;
+    printf("New nodo %d %s\n",new_nodo->sec, new_nodo->arq_executavel);
+    if(ready_queue->init == NULL){//empty queue
+        printf("queue init null :(\n");
+        ready_queue->init = new_nodo;
+    }else{
+        struct queue_nodo* tmp = ready_queue->init;
+        struct queue_nodo* previous = NULL;
+    
+        do{
+            if(tmp->sec <= new_nodo->sec){
+                previous = tmp;
+                tmp = tmp->next;   
+            }
+            else{
+                struct queue_nodo* aux = NULL;
+                if(previous != NULL){
+                    aux = previous->next;
+                    previous->next = new_nodo;
+                    new_nodo->next = aux;
+                }else{
+                    new_nodo->next = ready_queue->init;
+                    ready_queue->init = new_nodo;
+                }
+                break;
+            }
+        }while(tmp != NULL);
+        if(tmp == NULL){
+            previous->next = new_nodo;
+            new_nodo->next = NULL;
+        }
+    }
+    
+    return 1;
+
+}
+void print_queue(Queue* ready_queue){
+    struct queue_nodo* tmp = ready_queue->init;
+    if(!is_empty(ready_queue)){
+        do{
+            printf("Nodo %d -> ", tmp->sec); 
+            tmp = tmp->next;
+        }while(tmp != NULL);
+
+    }
+
+    printf("\n");
+
+}
 #endif
